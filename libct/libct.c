d2b2b17b3827511d27e2e1db2854596e210f67a0
Meh-Mehul <mehulsharmaiit@gmail.com>
2026-06-09 00:30:37 +0530

    next day...

---
+++ libct/libct.c
@@ -0,0 +1,19 @@
+#include "libct.h"
+#include<sys/socket.h>
+
+
+
+
+char* recv_file_n_save(int socket){
+  
+}
+
+
+bool validate_file(char* filepath){
+  
+}
+
+
+int compile_to_lib(char* filepath){
+  
+}
